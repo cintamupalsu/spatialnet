b597e3d23aa664c32937a358535ba4ede2b351c1
maulanamania <maulanamania@gmail.com>
2017-03-18 05:10:39 +0000

    Use SSL

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
-     # flash[:success] = "Welcome"
+      flash[:success] = "Welcome"
       
       redirect_to @user
     else
