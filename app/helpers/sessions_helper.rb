8aed23ad5bcbd4a0c72bd7c47d61779474fafe21
maulanamania <maulanamania@gmail.com>
2017-03-18 12:10:56 +0000

    finish user edit

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,11 +5,17 @@ module SessionsHelper
         session[:user_id]=user.id
     end
     
-    #Remeber a user in a persistent session
+    #Remember a user in a persistent session
     def remember(user)
         user.remember
         cookies.permanent.signed[:user_id]= user.id
         cookies.permanent[:remember_token]= user.remember_token
+    end
+    
+    #return tru if the given user is the current user.
+    def current_user?(user)
+        user==current_user
+        
     end
     
     # Returns the current logged-in user (if any).
@@ -48,4 +54,14 @@ module SessionsHelper
         @current_user = nil
     end
     
+    #Redirects to stored location (or to the default)
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+    
+    #Stores the URL trying to be accessed
+    def store_location
+        session[:forwarding_url] = request.url if request.get?
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Administrator",
+             email: "maulanamania@gmail.com",
+             password:              "t4jmahal",
+             password_confirmation: "t4jmahal",
+             admin: true)
+
+User.create!(name:  "Arief Maulana",
+             email: "maulanamania@zpost.plala.or.jp",
+             password:              "t4jmahal",
+             password_confirmation: "t4jmahal")
+
+#99.times do |n|
+#    name = Faker::Name.name
+#    email = "example-#{n+1}@railstutorial.org"
+#    password = "password"
+#    User.create!(name:  name,
+#                 email: email,
+#                 password:              password,
+#                 password_confirmation: password)
+#end
